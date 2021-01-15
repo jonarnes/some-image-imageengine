30882472d55963b6261b8fd6074be3ec1a19d930
Jon Arne S <seteras@gmail.com>
2021-01-15 13:05:28 +0100

    demopage fix

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import App from './App.svelte';
-const params = new URLSearchParams(window.location.search)
 
+//Read the URL parameters
+const params = new URLSearchParams(window.location.search)
 const app = new App({
 	target: document.body,
+
+	//Put the values of the URL params into props
 	props: {
 		title: params.get('title'),
 		desc: params.get('desc')
-
 	}
 });
-
 export default app;
